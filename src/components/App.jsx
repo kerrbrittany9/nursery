c6661aa874a5f6396f726aa57cc924d0beb29af5
Brittany Kerr <kerrbrittany9@gmail.com>
2017-09-15 08:44:08 -0700

    add form area to Catalog component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Catalog from "./Catalog";
 
 function App(props){
   return (
     <div>
       <h3>Pistils Nursery</h3>
       <p><em>Oregon's Specialty Plant Shop and Mercantile</em></p>
-  
+      <Catalog/>
     </div>
   );
 }
